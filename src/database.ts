e0529f79a2ff21a20272bc736e7bb5e873e5e7b9
Bernardo <bernardobregeron@gmail.com>
2026-06-18 15:37:42 -0300

    refactor: simplify database connection configuration by using DATABASE_URL

--- src/database.ts
+++ src/database.ts
@@ -4,15 +4,10 @@ import dotenv from "dotenv";
 dotenv.config({ quiet: true });
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-  maxLifetimeSeconds: 60,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
