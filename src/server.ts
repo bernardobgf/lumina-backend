ae8c60cb9546abf43027e57504ffef3ef2f0a454
Bernardo <bernardobregeron@gmail.com>
2026-06-22 20:50:58 -0300

    fixing cors

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "https://lumina-frontend-sand.vercel.app/",
+    credentials: true,
+  }),
+);
 
 //ROUTES
 app.use("/auth", authRouter);
